edf6db492109bdf7564a03a013180ebf15e3e069
ARCHIT <145273087+Archiitt@users.noreply.github.com>
2024-01-29 17:14:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,13 @@ const getChatResponse = async (incomingChatDiv) => {
     localStorage.setItem("all-chats", chatContainer.innerHTML);
     chatContainer.scrollTo(0, chatContainer.scrollHeight);
 }
-
+const copyResponse = (copyBtn) => {
+    // Copy the text content of the response to the clipboard
+    const reponseTextElement = copyBtn.parentElement.querySelector("p");
+    navigator.clipboard.writeText(reponseTextElement.textContent);
+    copyBtn.textContent = "done"; 
+    setTimeout(() => copyBtn.textContent = "content_copy", 1000);
+}
 
 
     
